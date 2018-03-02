aa89bce5e3074b5db74d838b9cb6a94a1db51c60
Jonas Höpfner <hoepfnej@swisslife-ad.de>
2018-03-02 17:46:08 +0100

    source parameters set to github

--- vpc-with-subnet/main.tf
+++ vpc-with-subnet/main.tf
@@ -3,15 +3,15 @@ provider "aws" {
 }
 
 module "vpc" {
-  #source = "github.com/hoepfnerj/terraform-modules.git//vpc?ref=v0.0.2"
-  source = "../../../terraform-modules/vpc"
+  source = "github.com/hoepfnerj/terraform-modules.git//vpc?ref=v0.0.2"
+  #source = "../../../terraform-modules/vpc"
   
   vpc_cidr_block = "10.200.0.0/16"
 }
 
 module "subnet" {
-  #source = "github.com/hoepfnerj/terraform-modules.git//subnet?ref=v0.0.2"
-  source = "../../../terraform-modules/subnet"
+  source = "github.com/hoepfnerj/terraform-modules.git//subnet?ref=v0.0.2"
+  #source = "../../../terraform-modules/subnet"
   
   vpc_id = "${module.vpc.vpc_id}"
   subnet_cidr = "${var.subnet_cidr}"
