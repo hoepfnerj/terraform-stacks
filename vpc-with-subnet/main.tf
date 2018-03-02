960f0f27498ac11d6751a1223fc3601c18a648f5
Jonas Höpfner <hoepfnej@swisslife-ad.de>
2018-03-02 17:51:39 +0100

    updated module versions

--- vpc-with-subnet/main.tf
+++ vpc-with-subnet/main.tf
@@ -3,14 +3,14 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "github.com/hoepfnerj/terraform-modules.git//vpc?ref=v0.0.2"
+  source = "github.com/hoepfnerj/terraform-modules.git//vpc?ref=v1.0.0"
   #source = "../../../terraform-modules/vpc"
   
   vpc_cidr_block = "10.200.0.0/16"
 }
 
 module "subnet" {
-  source = "github.com/hoepfnerj/terraform-modules.git//subnet?ref=v0.0.2"
+  source = "github.com/hoepfnerj/terraform-modules.git//subnet?ref=v1.0.0"
   #source = "../../../terraform-modules/subnet"
   
   vpc_id = "${module.vpc.vpc_id}"
