4d4df184f3469260d97e98b313f0d9b49e7b28a9
Jonas Höpfner <hoepfnej@swisslife-ad.de>
2018-03-02 17:28:59 +0100

    added vpc stack

---
+++ vpc-with-subnet/vars.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr_block" {
+  description = "The cidr block of the vpc"
+}
+
+variable "subnet_cidr" {
+  description = "The cidr of the subnet"
+}
+
+variable "subnet_availability_zone" {
+  description = "The availability zone of the subnet"
+}
